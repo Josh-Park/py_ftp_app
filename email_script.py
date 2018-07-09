aac817773fee17a6064f140c80874a9ff7fec513
Joshua Park <35139977+Josh-Park@users.noreply.github.com>
2018-07-09 12:55:19 -0500

    Can retrieve file from local FTP server and send emails with attachments

--- email_script.py
+++ email_script.py
@@ -4,27 +4,59 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-from_addr = "datalockertestuser@gmail.com"
-to_addr = "datalockertestuser@gmail.com"
+def send_email(from_addr, to_addr, subject, msg_body, login, password, SMTP_server = "smtp.gmail.com", port = 587):
+    # from_addr = "datalockertestuser@gmail.com"
+    # to_addr = "datalockertestuser@gmail.com"
 
-msg = MIMEMultipart()
+    msg = MIMEMultipart()
 
-msg["From"] = from_addr
-msg["To"] = to_addr
-msg["Subject"] = "Test email"
+    msg["From"] = from_addr
+    msg["To"] = to_addr
+    msg["Subject"] = subject
 
-print("Creating message body")
-msg_body = "I sent this bad boy from a Python script."
-msg.attach(MIMEText(msg_body, "plain"))
+    print("Creating message body")
+    msg.attach(MIMEText(msg_body, "plain"))
 
-print("Connecting to SMTP server")
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login(from_addr, "Datalocker1")
+    print("Connecting to SMTP server")
+    server = smtplib.SMTP(SMTP_server, port)
+    server.starttls()
+    server.login(from_addr, password)
 
-print("Sending email")
-text = msg.as_string()
+    print("Sending email")
+    text = msg.as_string()
 
-server.sendmail(from_addr, to_addr, text)
-server.quit()
-print("Email successfully sent")
+    server.sendmail(from_addr, to_addr, text)
+    server.quit()
+    print("Email successfully sent")
+
+def send_email_with_attachment(from_addr, to_addr, subject, msg_body, login, password, filename, filepath, SMTP_server = "smtp.gmail.com", port = 587):
+    msg = MIMEMultipart()
+
+    msg["From"] = from_addr
+    msg["To"] = to_addr
+    msg["Subject"] = subject
+
+    print("Creating message body")
+    msg.attach(MIMEText(msg_body, "plain"))
+
+    print("Attaching file")
+    attachment = open(filepath, "rb")
+
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header("Content-Disposition", "attachment; filename= %s" % filename)
+
+    msg.attach(part)
+
+    print("Connecting to SMTP server")
+    server = smtplib.SMTP(SMTP_server, port)
+    server.starttls()
+    server.login(from_addr, password)
+
+    print("Sending email")
+    text = msg.as_string()
+
+    server.sendmail(from_addr, to_addr, text)
+    server.quit()
+    print("Email successfully sent")
